52744be88b7342dbbe6e6527a765fe1ba5ce406d
Eliandro Freitas <eliandro.freitas@meuportfol.io>
2023-02-23 21:57:33 -0300

    removed test

--- Tests/CalculatorTests.cs
+++ Tests/CalculatorTests.cs
@@ -11,11 +11,4 @@ public class CalculatorTests
         var result = Calculator.Sum(1, 3);
         Assert.Equal(4, result);
     }
-
-    [Fact]
-    public void ShouldFail()
-    {
-        var result = Calculator.Sum(1, 3);
-        Assert.NotEqual(5, result);
-    }
 }
